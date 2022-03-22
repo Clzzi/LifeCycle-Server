5fa42c6c536a4150318b619acc56245b24ab13c9
Clzzi <clzzi@Clzziui-MacBookPro.local>
2022-03-22 11:03:39 +0900

    feat(base-setting): setting base in project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'mysql',
+        host: ConfigService.get('database.host'),
+        port: ConfigService.get('database.port'),
+        usernamek: ConfigService.get('database.user'),
+        password: ConfigService.get('database.password'),
+        database: ConfigService.get('database.name'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
