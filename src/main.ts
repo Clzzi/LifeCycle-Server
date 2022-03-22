e759d6de90bcaa7d28fbbe34a3dca4ef3b91daea
Clzzi <clzzi@Clzziui-MacBookPro.local>
2022-03-22 13:33:46 +0900

    feat(base-setting): main setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from './common/pipes/validation.pipe';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: NestExpressApplication =
+    await NestFactory.create<NestExpressApplication>(AppModule, {
+      cors: false,
+    });
+  const port: number = app.get(ConfigService).get<number>('PORT');
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(port, '0.0.0.0');
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
