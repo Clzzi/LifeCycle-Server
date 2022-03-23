6bc1f6afaf37c0be409058049bad694043c65a95
Clzzi <clzzi@Clzziui-MacBookPro.local>
2022-03-23 10:54:31 +0900

    feat(getUserByUserId): make getUserInfo by UserID

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryColumn({ length: 40, unique: true })
+  @PrimaryColumn({ name: 'user_id', length: 40, unique: true })
   userId: string;
 
   @Column({ length: 10 })
